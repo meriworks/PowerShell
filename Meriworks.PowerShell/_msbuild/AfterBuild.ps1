b6da32680372952da23a8fd577b271db0a919522
internal\dan <dan@meriworks.se>
2016-11-09 11:27:50 +0100

    Fix signing of powershell scripts

--- Meriworks.PowerShell/_msbuild/AfterBuild.ps1
+++ Meriworks.PowerShell/_msbuild/AfterBuild.ps1
@@ -3,5 +3,4 @@
 #Add powershell statements that should be executed after the build
 
 Write-Host "Signing scripts"
-SignScript (join-path $projectDir "nuspec/tools/install.ps1")
-SignScript (join-path $projectDir "nuspec/tools/Meriworks.PowerShell.psd1")
+SignScriptsInFolder (join-path $projectDir "nuspec/tools")
