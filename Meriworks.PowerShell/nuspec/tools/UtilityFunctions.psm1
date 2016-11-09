d938b4c48b1ef985abe8b8c7ec516d390bc1f011
internal\dan <dan@meriworks.se>
2016-11-09 10:05:06 +0100

    missed the utility functions file

---
+++ Meriworks.PowerShell/nuspec/tools/UtilityFunctions.psm1
@@ -0,0 +1,12 @@
+﻿Write-Host "Information: Documentation for Meriworks.PowerShell can be found @ https://github.com/meriworks/PowerShell"
+
+#Runs the robocopy command and fixes the last exit code so it don't messes up the powershell invoke
+function run-robocopy {
+	robocopy $args
+	if($lastexitcode -le 7) {
+		#need to set the global lastexitcode to make it work with any calling scripts
+		$global:lastexitcode=$null
+	} else {
+		throw "Robocopy had some failures."
+	}
+}
